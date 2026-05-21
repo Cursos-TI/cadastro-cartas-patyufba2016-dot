567aff618ad4262f13fd4a88d1d729c5e2a25d9d
patyufba2016-dot <patyufba2016@gmail.com>
2026-05-21 01:50:45 +0000

    Entrada das variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,9 +5,15 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+    printf("Carta Um\n");
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  printf("olpa, Paty!\n");
-
+    char estado
+    char codigo[5]
+    char cidade
+    int populacao
+    float area
+    float pib
+    int pontos
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
