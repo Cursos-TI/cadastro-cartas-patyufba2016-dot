6d9d146e0fccb3c92d29d4cfd25a062160193db0
patyufba2016-dot <patyufba2016@gmail.com>
2026-05-27 22:53:51 +0000

    ok

---
+++ 2505EXERC.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+int temperatura=28;
+int resultado;
+
+temperatura > 30?  printf ("calor \n") : printf ("frio  \n");
+
+
+return 0;
+}
