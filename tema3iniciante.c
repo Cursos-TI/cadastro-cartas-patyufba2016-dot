a9589b163c7f971713b715b6e2e0c10c67359f76
patyufba2016-dot <patyufba2016@gmail.com>
2026-05-27 22:25:23 +0000

    FETIO

--- tema3iniciante.c
+++ tema3iniciante.c
@@ -1,40 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
 
 // Área para definição das variáveis carta 1
-char estado [30];
-char codigo[5];
-char cidade[50];
+char pais [30];
 int populacao;
 float area;
 float pib;
 int pontostur;
 int densidadepopulacional;
-int pibpercapta;
+
 
 // Área para definição das variáveis carta 2
-char estado1[30];
-char codigo1[5];
-char cidade1[50];
+char pais1[30];
 int populacao1;
 float area1;
 float pib1;
 int pontostur1;
 int densidadepopulacional1;
-int pibpercapta1;
+int opcao;
+
 
 // Área para entrada de dados carta 1
 printf("--- CADASTRO DA CARTA 1 ---\n");
     
-printf("Digite o estado :\n");  
-scanf("%s", estado);
+printf("Digite o pais :\n");
+scanf("%s", pais);
 
-printf("Digite o código formado pela letra inicial do estado e um número de 01 a o4 :\n");  
-scanf("%s", codigo);
-
-printf("Digite o nome da cidade :\n");  
-scanf("%s", cidade);
 
 printf("Digite o número de habitantes da cidade :\n");
 scanf("%d", &populacao);
@@ -49,19 +43,13 @@ printf("Digite a quantidade de pontos turísticos na cidade :\n");
 scanf("%d", &pontostur);
 
 densidadepopulacional = populacao / area;
-pibpercapta = pib /populacao;
+
 
 // Área para entrada de dados carta 2
 printf("--- CADASTRO DA CARTA 2 ---\n");
     
-printf("Digite o estado1 :\n");  
-scanf("%s", estado1);
-
-printf("Digite o código formado pela letra inicial do estado e um número de 01 a o4 :\n");  
-scanf("%s", codigo1);
-
-printf("Digite o nome da cidade :\n");  
-scanf("%s", cidade1);
+printf("Digite o pais :\n");
+scanf("%s", pais1);
 
 printf("Digite o número de habitantes da cidade :\n");
 scanf("%d", &populacao1);
@@ -76,13 +64,37 @@ printf("Digite a quantidade de pontos turísticos na cidade :\n");
 scanf("%d", &pontostur1);
 
 densidadepopulacional1 = populacao1 / area1;
-pibpercapta1 = pib1 /populacao1;
 
-//comparação de atributos
-if (populacao > populacao1) {
-printf ("Resultado:Carta1 venceu! \n");
-}else{ printf ("Resultado: Carta 2 venceu! \n'");
-}
+//  menu interativo
+    printf("Escolha o atributo que deseja comparar: \n");
+    printf("1. Compare atributos de População\n");
+    printf("2. Compare atributos de áreas\n");
+    printf("3. Compare atributos de pib \n");
+    printf("4. Compare atributos de Pontos Turíiticos \n");
+    printf("3. Compare Denisdade Demográfica \n");
+    scanf("%d", &opcao);
+
+    
+    switch (opcao)
+    {
+    case 1 :
+        printf("País %s e País %s  \n", pais,pais1);
+        printf("O atributo comparado é:População \n ");
+        printf("A população de %s é %d habitantes \n",pais,populacao);
+        printf("A população de %s é %d habitantes \n",pais1,populacao1);
+        
+        if (populacao == populacao1) {
+        printf("Empate!!! \n");
+      } else if (populacao > populacao1)
+{        printf("País %s ganhou! \n", pais);
+          } else{printf("País %s ganhou! \n",pais1);}
+
+        break;
+    
+    default:
+    { printf("Opção inválisa");}
+        break;
+    }
 
 return 0;
 }
